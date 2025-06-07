50830f0b2276cb4b6e8f21cdd8dbf353049f94dd
Matthew LettucePie <thelettucepie@gmail.com>
2025-06-07 12:10:24 -0400

    customer audio sounds

--- scenes/game/customer.gd
+++ scenes/game/customer.gd
@@ -1,6 +1,13 @@
-extends Node2D
+extends Area2D
 class_name Customer
+
 
 @export var customer_name : String
 @export var orders : Array[PackedStringArray] = []
 @export var consecutive_orders : int = 1
+
+
+
+func _ready():
+	if !is_in_group("Target_Area"):
+		add_to_group("Target_Area")
