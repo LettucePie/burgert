4c2cf6308d8b87cfe63a838d1a449dca2e9bbab2
Matthew LettucePie <thelettucepie@gmail.com>
2025-06-23 17:03:55 -0400

    setup framework for customers entering and exiting

--- scenes/game/customer.gd
+++ scenes/game/customer.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Customer
 
+signal customer_finished()
+
 
 @export var customer_name : String
 @export var orders : Array[PackedStringArray] = []
@@ -13,10 +15,14 @@ class_name Customer
 @export var enter_pos : Vector2
 @export var stay_pos : Vector2
 @export var exit_pos : Vector2
-var active : bool = false
+@export var enter_speed : float = 8
+@export var munch_seconds : float = 1.0
+@export var exit_speed : float = 8
 var tic : int = 0
 var default_y : float = 0
-var munching : bool = false
+enum CUSTOMER_STATE {Entering, Waiting, Munching, Leaving, Gone}
+var status : CUSTOMER_STATE = CUSTOMER_STATE.Gone
+var munch_timer : int = 0
 
 
 func _ready():
@@ -25,10 +31,22 @@ func _ready():
 	default_y = $character.position.y
 
 
-func set_active(tf : bool):
-	active = tf
-	if tf:
+func set_state(new_state : CUSTOMER_STATE):
+	if new_state == CUSTOMER_STATE.Entering:
+		self.position = enter_pos
+		burger_portal_sprite.hide()
+	if new_state == CUSTOMER_STATE.Waiting:
+		self.position = stay_pos
+		burger_portal_sprite.show()
 		play_greeting()
+	if new_state == CUSTOMER_STATE.Munching:
+		burger_portal_sprite.hide()
+		munch.show()
+		munch_timer = Time.get_ticks_msec() + (munch_seconds * 1000)
+	if new_state == CUSTOMER_STATE.Leaving:
+		munch.hide()
+	if new_state == CUSTOMER_STATE.Gone:
+		emit_signal("customer_finished")
 
 
 func play_greeting():
@@ -36,7 +54,11 @@ func play_greeting():
 
 
 func _physics_process(delta):
-	if active:
+	if status == CUSTOMER_STATE.Entering:
+		self.position = self.position.move_toward(stay_pos, enter_speed)
+		if self.position.distance_to(stay_pos) < 1.5:
+			set_state(CUSTOMER_STATE.Waiting)
+	if status == CUSTOMER_STATE.Waiting:
 		tic += 1
 		if tic >= 60:
 			tic = 0
@@ -44,3 +66,17 @@ func _physics_process(delta):
 				$character.position.y += 2
 			else:
 				$character.position.y = default_y
+	if status == CUSTOMER_STATE.Munching:
+		tic += 2
+		if tic >= 60:
+			tic = 0
+			if $character.position.y == default_y :
+				$character.position.y += 4
+			else:
+				$character.position.y = default_y
+		if Time.get_ticks_msec() >= munch_timer:
+			set_state(CUSTOMER_STATE.Leaving)
+	if status == CUSTOMER_STATE.Leaving:
+		self.position = self.position.move_toward(exit_pos, exit_speed)
+		if self.position.distance_to(exit_pos) < 1.5:
+			set_state(CUSTOMER_STATE.Gone)
