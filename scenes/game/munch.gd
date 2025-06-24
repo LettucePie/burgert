e8eda8b3a9f4c96ac5b27260aa0f016389587347
Matthew LettucePie <thelettucepie@gmail.com>
2025-06-24 10:54:24 -0400

    added munch sound

--- scenes/game/munch.gd
+++ scenes/game/munch.gd
@@ -3,6 +3,7 @@ class_name Munch
 
 @onready var left : AnimatedSprite2D = $left
 @onready var right : AnimatedSprite2D = $right
+@onready var sound : AudioStreamPlayer2D = $AudioStreamPlayer2D
 var options : Array = ["_1", "_2", "_3"]
 
 var playing : bool = false
@@ -19,6 +20,7 @@ func play():
 	playing = true
 	left.play("crumble" + options.pick_random())
 	right.play("crumble" + options.pick_random())
+	sound.play()
 
 
 func stop():
@@ -26,6 +28,7 @@ func stop():
 	self.hide()
 	left.stop()
 	right.stop()
+	sound.stop()
 
 
 func _on_finish(side : AnimatedSprite2D):
