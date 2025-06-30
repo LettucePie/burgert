493621c9758d3cd313ad97c04dc93d679284a270
Matthew LettucePie <thelettucepie@gmail.com>
2025-06-30 11:35:54 -0400

    failed attempt to fix html audio

--- scenes/music.gd
+++ scenes/music.gd
@@ -4,15 +4,25 @@ class_name Music
 
 @export var menu : AudioStreamMP3
 @export var tracks : Array[AudioStreamMP3]
+@export var menu_lq : AudioStreamMP3
+@export var tracks_lq : Array[AudioStreamMP3]
 @onready var anim : AnimationPlayer = $anim
 
 enum STATE {MENU, PLAY, PAUSE}
 var current_state : STATE = STATE.MENU
+var web : String = ""
+
+
+func _ready():
+	if OS.has_feature("web"):
+		web = "_WEB"
+		stream = menu_lq
+		anim.play("intro_ramp_WEB")
 
 
 func set_state(new_state : STATE):
 	if new_state == STATE.MENU:
-		anim.play("intro_ramp")
+		anim.play("intro_ramp" + web)
 	if new_state == STATE.PLAY:
 		if current_state == STATE.MENU:
 			anim.play("start_play")
@@ -26,6 +36,8 @@ func set_state(new_state : STATE):
 
 func set_track_random():
 	var options = tracks.duplicate()
+	if OS.has_feature("web"):
+		options = tracks_lq.duplicate()
 	if options.has(stream):
 		options.erase(stream)
 	stream = options.pick_random()
