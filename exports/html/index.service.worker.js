942aa6f9cfec5e0b241059fcd3d030d36ddc1763
Matthew LettucePie <thelettucepie@gmail.com>
2025-06-30 03:09:19 -0400

    to fix audio on html i need to upgrade godot or use lower quality audio files i think

--- exports/html/index.service.worker.js
+++ exports/html/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1751265641|304695491";
+const CACHE_VERSION = "1751267331|26278006";
 const CACHE_PREFIX = "burgert-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
