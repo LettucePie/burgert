b853fecc9c0e0eb015c6f674a54c3581bf455ed7
Matthew LettucePie <thelettucepie@gmail.com>
2025-07-13 10:40:04 -0400

    new export

--- exports/html/index.service.worker.js
+++ exports/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751423481|347197525';
+const CACHE_VERSION = '1752417571|4963701989';
 /** @type {string} */
 const CACHE_PREFIX = 'burgert-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
