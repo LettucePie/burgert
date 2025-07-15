81778ac3f2ac2b0f28c9f3be2f0de95c15bbd216
Matthew LettucePie <thelettucepie@gmail.com>
2025-07-15 14:49:45 -0400

    fixed double-tapping menu fx

--- scenes/ui/menu_flair.gd
+++ scenes/ui/menu_flair.gd
@@ -5,17 +5,16 @@ class_name MenuFlair
 
 @export var splat_vfx : PackedScene
 
+var last_button_splattered : Control = null
+
 func _ready():
 	for sb in splat_buttons:
 		sb.focus_entered.connect(splat_button_focused.bind(sb))
-	#for wb in wobble_buttons:
-		#wb.focus_entered.connect(wobble_button_focused.bind(wb))
 
 
 func splat_button_focused(control : Control):
-	var new_splat = splat_vfx.instantiate()
-	new_splat.activate_on_target(control)
-
-
-func wobble_button_focused(button : Button):
-	print("Wobble Button Focused: ", button)
+	if last_button_splattered != control:
+		var new_splat = splat_vfx.instantiate()
+		new_splat.activate_on_target(control)
+		last_button_splattered = control
+	
