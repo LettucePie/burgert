bb2551982765e91f1cb8db576d69671b5714988e
Matthew LettucePie <thelettucepie@gmail.com>
2025-07-21 09:08:22 -0400

    customer-dex reveals data

--- scenes/ui/customer_dex.gd
+++ scenes/ui/customer_dex.gd
@@ -4,7 +4,8 @@ class_name CustomerDex
 @onready var name_label : Label = $Panel/content/picture_bg/picture_overlay/customer_name
 @onready var pic_rect : TextureRect = $Panel/content/picture_bg/customer_pic
 @onready var stats_label : Label = $Panel/content/vbox/stats/value_label
-@onready var description : Label = $Panel/content/vbox/description
+@onready var description_a : Label = $Panel/content/vbox/description_a
+@onready var description_b : Label = $Panel/content/vbox/description_b
 @onready var page_label : Label = $Panel/content/pgnum
 
 @export var customer_names_internal : PackedStringArray = [
@@ -18,64 +19,68 @@ class_name CustomerDex
 	"Jimmie", "Mr. Pink", "Macho Liam"
 ]
 @export var customer_images : Array[Texture2D]
-@export var customer_descriptions : PackedStringArray = [
-	"Species: Lima Bean\nHeight: 4'10\nFavorite Ingredient: Lettuce\n\
-	Can often be seen around the local theater... like all the time. Probably \
+@export var customer_descriptions_a : PackedStringArray = [
+	"Species: Lima Bean\nHeight: 4'10\nFavorite Ingredient: Lettuce",
+	"Species: Plorb\nHeight: 6'7\nFavorite Ingredient: Cheese",
+	"Species: Bronk\nHeight 5'4\nFavorite Ingredient: Meat",
+	"Species: Plorb\nHeight: 6'0\nFavorite Ingredient: Meat",
+	"Species: Unknown\nHeight: 5'10\nFavorite Ingredient: N/A",
+	"Species: Bronk\nHeight: 5'2\nFavorite Ingredient: Time",
+	"Species: Djinn\nHeight: 5'11\nFavorite Ingredient: CHEESE",
+	"Species: Lima Bean\nHeight: 5'1\nFavorite Ingred- Mustard it's Mustard",
+	"Species: Djinn\nHeight: 6'8\nFavorite Ingredient: Bread",
+	"Species: Lima Bean\nHeight: 4'10 or 5'6 with hair\nFavorite Ingredient: \
+	Ketchup",
+	"Species: Australien\nHeight: 5'10\nFavorite Ingredient: N/A",
+	"Species: Lima Bean\nHeight: 5'3\nFavorite Ingredient: Burgers",
+	"Species: Fish\nHeight: 0'2 without the apparatus\nFavorite Ingredient: \
+	Bread Crumbs",
+	"Species: Bronk\nHeight: 5'11\nFavorite Ingredient: Paint",
+	"Species: GetBentOS\nHeight: Standard\nFavorite Ingredient: Lettuce",
+	"Species: Plorb\nHeight: 6'7\nFavorite Ingredient: Tomato",
+	"Species: Djinn\nHeight: 5'4\nFavorite Ingredient: Meat"
+]
+@export var customer_descriptions_b : PackedStringArray = [
+	"Can often be seen around the local theater... like all the time. Probably \
 	lives there. Is insistant that actors must perform their own stunts.",
-	"Species: Plorb\nHeight: 6'7\nFavorite Ingredient: Cheese\n\
-	Extemely lonely and worried about what others think. This dude won't come \
+	"Extemely lonely and worried about what others think. This dude won't come \
 	to the counter unless we've made eye-contact at least 5 times.",
-	"Species: Bronk\nHeight 5'4\nFavorite Ingredient: Meat\n\
-	Always a good time with Ted. This dude's so freakin funny. Clever too. \
+	"Always a good time with Ted. This dude's so freakin funny. Clever too. \
 	I wish I could orchestrate jokes the way he does.\n\nShame about the \
 	depression.",
-	"Species: Plorb\nHeight: 6'0\nFavorite Ingredient: Meat\n\
-	Local handy-man. Probably has fixed everything in here at one point... \
+	"Local handy-man. Probably has fixed everything in here at one point... \
 	except for his meal.",
-	"Species: Unknown\nHeight: 5'10\nFavorite Ingredient: N/A\n\
-	Dramatic, Sporadic, Confusing... I have no idea where to begin with this \
+	"Dramatic, Sporadic, Confusing... I have no idea where to begin with this \
 	one. I don't even know how to start a conversation through their endless \
 	monologue.",
-	"Species: Bronk\nHeight: 5'2\nFavorite Ingredient: Time\n\
-	Over time, we have come to a mutual agreement... you see, I too really \
+	"Over time, we have come to a mutual agreement... you see, I too really \
 	want to get this over with. The sooner I'm done, the sooner you're gone.",
-	"Species: Djinn\nHeight: 5'11\nFavorite Ingredient: CHEESE\n\
-	Honestly a really nice guy and strangely comforting to talk to. \
+	"Honestly a really nice guy and strangely comforting to talk to. \
 	I can actively sense that they're listening, and they never interrupt \
 	unless I'm struggling to find my words. Also the dude looks like cheese.",
-	"Species: Lima Bean\nHeight: 5'1\nFavorite Ingred- Mustard it's Mustard\n\
-	Always comes in after closing the theater, then continues to slam down \
+	"Always comes in after closing the theater, then continues to slam down \
 	hand-fulls of mustard. I mean I make it fresh and have to throw it away \
 	anyway... still it's unnerving.",
-	"Species: Djinn\nHeight: 6'8\nFavorite Ingredient: Bread\n\
-	One of my favorites. She's in and out, has simple requests, and manages \
+	"One of my favorites. She's in and out, has simple requests, and manages \
 	to make an enchanting breeze as she dances out the door.",
-	"Species: Lima Bean\nHeight: 4'10 or 5'6 with hair\nFavorite Ingredient: \
-	Ketchup\nI try to be an honest chef. I make food how the people want it, \
+	"Ketchup\nI try to be an honest chef. I make food how the people want it, \
 	it makes me happy. This dude though, this dude is a freak.",
-	"Species: Australien\nHeight: 5'10\nFavorite Ingredient: N/A\n\
-	I think they're trying to make their own sandwhich somewhere, I don't get \
+	"I think they're trying to make their own sandwhich somewhere, I don't get \
 	it. Just let me make your sandwhich for you. It's my whole job. I'm not \
 	a grocery store!",
-	"Species: Lima Bean\nHeight: 5'3\nFavorite Ingredient: Burgers\n\
-	Just order a triple. I can do that. Nothing bad will happen. You will get \
+	"Just order a triple. I can do that. Nothing bad will happen. You will get \
 	the same amount of food, and be on your way to continue deliveries.\n\n\
 	oh wait... you're just wasting your time here aren't you...",
-	"Species: Fish\nHeight: 0'2 without the apparatus\nFavorite Ingredient: \
-	Bread Crumbs\nWhen people don't have time to feed fishes at the pond... the \
+	"Bread Crumbs\nWhen people don't have time to feed fishes at the pond... the \
 	fish, find a way.",
-	"Species: Bronk\nHeight: 5'11\nFavorite Ingredient: Paint\n\
-	He's about to go on a date... I know it. He gets a haircut, then comes \
+	"He's about to go on a date... I know it. He gets a haircut, then comes \
 	here to eat just before meeting up with is date. I'm guessing he is worried \
 	about how he eats or gets too nervous and ends up starving.",
-	"Species: GetBentOS\nHeight: Standard\nFavorite Ingredient: Lettuce\n\
-	Sarcastic and Rude... as intended. Sometimes I convince myself the \
+	"Sarcastic and Rude... as intended. Sometimes I convince myself the \
 	\"Thank you have a nice day\" isn't disingenuous.",
-	"Species: Plorb\nHeight: 6'7\nFavorite Ingredient: Tomato\n\
-	Extremely physically slow. Works at the therapy center, so I'm guessing \
+	"Extremely physically slow. Works at the therapy center, so I'm guessing \
 	he has trained himself to be this slow and balanced.",
-	"Species: Djinn\nHeight: 5'4\nFavorite Ingredient: Meat\n\
-	Will not stop pestering me about protein intake. I'm sure it's important \
+	"Will not stop pestering me about protein intake. I'm sure it's important \
 	but there are other vitamins out there too.\n\nUsually hangs out at the gym \
 	with that other guy..."
 ]
@@ -94,11 +99,17 @@ func set_display_names(strings : Array):
 		if s is String:
 			customer_names_display.append(s)
 
-func set_customer_descriptions(strings : Array):
-	customer_descriptions.clear()
+func set_customer_descriptions_a(strings : Array):
+	customer_descriptions_a.clear()
 	for s in strings:
 		if s is String:
-			customer_descriptions.append(s)
+			customer_descriptions_a.append(s)
+
+func set_customer_descriptions_b(strings : Array):
+	customer_descriptions_b.clear()
+	for s in strings:
+		if s is String:
+			customer_descriptions_b.append(s)
 
 ####
 #### End Multi Lang Setters
@@ -127,18 +138,32 @@ func _on_pagebutton_pressed(dir: int) -> void:
 
 func _load_page(target : int):
 	current_page = target
-	name_label.text = customer_names_display[target]
-	pic_rect.texture = customer_images[target]
+	name_label.text = "UNKNOWN"
+	pic_rect.texture = customer_image_unknown
+	stats_label.text = "0\n0\n0"
+	description_a.text = "Data Unavailable\n - Serve more orders..."
+	description_b.text = "Description Unavailable\n - Serve 10 or more Fantastic Orders"
 	var stats : Play.Stats.CustomerStat = customer_stats[target]
 	stats_label.text = \
 		str(stats.get_fantastic_orders()) + "\n" + \
 		str(stats.get_satisfactory_orders()) + "\n" + \
 		str(stats.get_disappointing_orders())
-	description.text = customer_descriptions[target]
-	description.visible_ratio = 0.0
+	if stats.get_orders_served() >= 1:
+		pic_rect.texture = customer_images[target]
+	if stats.get_orders_served() >= 5 \
+	or stats.get_fantastic_orders() >= 2 \
+	or stats.get_disappointing_orders() >= 1:
+		name_label.text = customer_names_display[target]
+	if stats.get_fantastic_orders() >= 4 \
+	or stats.get_satisfactory_orders() >= 8 \
+	or stats.get_orders_served() >= 15:
+		description_a.text = customer_descriptions_a[target]
+	if stats.get_fantastic_orders() >= 10:
+		description_b.text = customer_descriptions_b[target]
+	description_b.visible_ratio = 0.0
 	page_label.text = str(target + 1) + "\n--\n" + str(customer_names_internal.size())
 
 
 func _physics_process(delta: float) -> void:
-	if description.visible_ratio < 1.0:
-		description.visible_ratio += 0.018
+	if description_b.visible_ratio < 1.0:
+		description_b.visible_ratio += 0.018
