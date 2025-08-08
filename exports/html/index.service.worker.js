0811f92707236d24247eb6b851598110ef82d77a
Matthew LettucePie <thelettucepie@gmail.com>
2025-08-08 01:12:15 -0400

    export post-merge

--- exports/html/index.service.worker.js
+++ exports/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754629466|5849324778';
+const CACHE_VERSION = '1754629838|79347486';
 /** @type {string} */
 const CACHE_PREFIX = 'burgert-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
