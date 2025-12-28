5d906d2de3ea79246a5230db34ba82845eecd5e5
Matthew LettucePie <thelettucepie@gmail.com>
2025-12-28 14:40:37 -0500

    creating test env

--- scenes/ui/results.gd
+++ scenes/ui/results.gd
@@ -3,13 +3,13 @@ class_name Results
 
 signal finished_results()
 
-@onready var order_count : Label = $Panel/VBoxContainer/ordercount
-@onready var accuracy : Label = $Panel/VBoxContainer/entry_2/accuracy
-@onready var score : Label = $Panel/VBoxContainer/entry_3/score
-@onready var finish_button : Button = $Panel/VBoxContainer/finish
+@onready var order_count : Label = $Panel/ordercount
+@onready var accuracy : Label = $Panel/accuracy
+@onready var score : Label = $Panel/score
+@onready var finish_button : Button = $Panel/finish
 
 var stat_order_count : int = 0
-
+var accuracy_percent : float = 0.0
 
 @onready var anim_tree : AnimationTree = $AnimationTree
 @onready var anim_play : AnimationPlayer = $AnimationPlayer
@@ -17,15 +17,28 @@ var animating : bool = false
 var animation_step : int = 0
 
 
+##TESTING
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE and OS.has_feature("editor"):
+			print("RUNNING TEST")
+			display_results(
+				PackedFloat32Array([0.333, 0.5, 0.1, 1.0, 0.5, 0.67, 0.8, 0.333]), 
+				randi_range(5, 100))
+
 
 func _ready():
+	if anim_tree.anim_player != anim_play.get_path():
+		anim_tree.anim_player = anim_play.get_path()
 	self.hide()
 
 
 func display_results(accuracies : PackedFloat32Array, game_score : int):
 	#order_count.text = str(accuracies.size())
+	stat_order_count = 0
+	accuracy_percent = 0.0
+	##
 	stat_order_count = accuracies.size()
-	var accuracy_percent : float = 0.0
 	for a in accuracies:
 		print("Accuracy: ", a)
 		accuracy_percent += a
@@ -36,6 +49,7 @@ func display_results(accuracies : PackedFloat32Array, game_score : int):
 	animating = true
 	animation_step = 0
 	finish_button.grab_focus()
+	anim_play.play("alarm")
 
 
 func _on_finish_pressed():
