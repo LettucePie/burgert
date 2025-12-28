1290b32e3602937a20b1397a62edf88d78e686a1
Matthew LettucePie <thelettucepie@gmail.com>
2025-12-28 15:08:35 -0500

    starting to setup animtree

--- scenes/ui/results.gd
+++ scenes/ui/results.gd
@@ -10,9 +10,11 @@ signal finished_results()
 
 var stat_order_count : int = 0
 var accuracy_percent : float = 0.0
+var score_total : int = 0
 
 @onready var anim_tree : AnimationTree = $AnimationTree
 @onready var anim_play : AnimationPlayer = $AnimationPlayer
+var anim_state : AnimationNodeStateMachinePlayback
 var animating : bool = false
 var animation_step : int = 0
 
@@ -30,6 +32,7 @@ func _input(event: InputEvent) -> void:
 func _ready():
 	if anim_tree.anim_player != anim_play.get_path():
 		anim_tree.anim_player = anim_play.get_path()
+	anim_state = anim_tree.get("parameters/playback")
 	self.hide()
 
 
@@ -37,6 +40,7 @@ func display_results(accuracies : PackedFloat32Array, game_score : int):
 	#order_count.text = str(accuracies.size())
 	stat_order_count = 0
 	accuracy_percent = 0.0
+	score_total = game_score
 	##
 	stat_order_count = accuracies.size()
 	for a in accuracies:
@@ -49,9 +53,18 @@ func display_results(accuracies : PackedFloat32Array, game_score : int):
 	animating = true
 	animation_step = 0
 	finish_button.grab_focus()
-	anim_play.play("alarm")
+	anim_state.travel("alarm")
+	#anim_play.play("alarm")
+	#anim_tree.trav
+	#anim_tree.
 
 
 func _on_finish_pressed():
 	if !animating:
 		emit_signal("finished_results")
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	print("Animation Finished: ", anim_name)
+	if anim_name == "alarm":
+		anim_state.travel("panel_enter")
