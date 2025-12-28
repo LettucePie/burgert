900e83243bfbc42875ccd167aa626b36b4e399e6
Matthew LettucePie <thelettucepie@gmail.com>
2025-12-27 19:57:46 -0500

    order count impact animation

--- scenes/ui/results.gd
+++ scenes/ui/results.gd
@@ -3,10 +3,13 @@ class_name Results
 
 signal finished_results()
 
-@onready var order_count : Label = $Panel/VBoxContainer/entry_1/ordercount
+@onready var order_count : Label = $Panel/VBoxContainer/ordercount
 @onready var accuracy : Label = $Panel/VBoxContainer/entry_2/accuracy
 @onready var score : Label = $Panel/VBoxContainer/entry_3/score
 @onready var finish_button : Button = $Panel/VBoxContainer/finish
+
+var stat_order_count : int = 0
+
 
 @onready var anim_tree : AnimationTree = $AnimationTree
 @onready var anim_play : AnimationPlayer = $AnimationPlayer
@@ -14,12 +17,14 @@ var animating : bool = false
 var animation_step : int = 0
 
 
+
 func _ready():
 	self.hide()
 
 
 func display_results(accuracies : PackedFloat32Array, game_score : int):
-	order_count.text = str(accuracies.size())
+	#order_count.text = str(accuracies.size())
+	stat_order_count = accuracies.size()
 	var accuracy_percent : float = 0.0
 	for a in accuracies:
 		print("Accuracy: ", a)
