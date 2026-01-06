c4bca004303579a57d50a66131644f31b2a9fdd9
Matthew LettucePie <thelettucepie@gmail.com>
2026-01-05 19:55:14 -0500

    working on money entering register sequence

--- scenes/ui/results.gd
+++ scenes/ui/results.gd
@@ -17,10 +17,14 @@ var tick : int = 0
 @onready var jitter : JitterDialog = $AudioStreamPlayer/JitterDialog
 var typing_scorelabel : bool = false
 var typing_scorelabel_charcount : int = -1
+@onready var money_sprite : Money_FX = $assets/money_sprite
+@onready var register : TextureRect = $Panel/register
+var counting_money : bool = false
 
 var result_accuracies : PackedFloat32Array
 var result_order_count : int = 0
 var result_percent : float = 0.0
+var result_scores : PackedInt32Array
 var result_total : int = 0
 
 @onready var anim_play : AnimationPlayer = $AnimationPlayer
@@ -34,6 +38,7 @@ var timeline : PackedStringArray = [
 	"!spawn_receipts",
 	"score_enter",
 	"register_enter",
+	"!count_money",
 	"!done"
 ]
 
@@ -52,38 +57,75 @@ func _ready():
 	self.hide()
 
 
+func _process_receipts() -> void:
+	tick += 2
+	if tick > 30:
+		tick = 0
+		var new_receipt : Receipt_FX = receipt_sprite.duplicate()
+		receipt_tray.add_child(new_receipt)
+		new_receipt.play_bop()
+		var temporary_percent : float = 0.0
+		print("step: ", spawning_receipt_count)
+		var index = result_order_count - spawning_receipt_count
+		print("index: ", index)
+		for i in index:
+			print(i, ": ", result_accuracies[i])
+			temporary_percent += result_accuracies[i]
+		temporary_percent /= index
+		print("TempPercent: ", temporary_percent)
+		if index > 0 :
+			order_count.text = str(index)
+			accuracy.text = str(snapped((temporary_percent * 100), 0.01)) + "%"
+		spawning_receipt_count -= 1
+		if spawning_receipt_count < 0:
+			spawning_receipts = false
+			accuracy.text = str(snapped((result_percent * 100), 0.01)) + "%"
+			next_animation_step()
+
+
+func _process_typing() -> void:
+	if typing_scorelabel_charcount != scorelabel.visible_characters:
+		typing_scorelabel_charcount = scorelabel.visible_characters
+		if scorelabel.text.right(1) != " ":
+			jitter.jitter()
+		if typing_scorelabel_charcount <= -1:
+			typing_scorelabel = false
+
+
+func _process_money() -> void:
+	tick += 2
+	if tick > 30:
+		tick = 0
+		var new_money : Money_FX = money_sprite.duplicate()
+		register.add_child(new_money)
+		print("Calculate loss or gain")
+		new_money.play_anim("add")
+		var temporary_score : int = 0
+		print("step: ", spawning_receipt_count)
+		var index = result_order_count - spawning_receipt_count
+		print("index: ", index)
+		for i in index:
+			print(i, ": ", result_accuracies[i])
+			#temporary_percent += result_accuracies[i]
+		#temporary_percent /= index
+		#print("TempPercent: ", temporary_percent)
+		if index > 0 :
+			pass
+			#order_count.text = str(index)
+			#accuracy.text = str(snapped((temporary_percent * 100), 0.01)) + "%"
+		spawning_receipt_count -= 1
+		if spawning_receipt_count < 0:
+			counting_money = false
+			next_animation_step()
+
+
 func _physics_process(delta: float) -> void:
 	if spawning_receipts and spawning_receipt_count >= 0:
-		tick += 2
-		if tick > 30:
-			tick = 0
-			var new_receipt : Receipt_FX = receipt_sprite.duplicate()
-			receipt_tray.add_child(new_receipt)
-			new_receipt.play_bop()
-			var temporary_percent : float = 0.0
-			print("step: ", spawning_receipt_count)
-			var index = result_order_count - spawning_receipt_count
-			print("index: ", index)
-			for i in index:
-				print(i, ": ", result_accuracies[i])
-				temporary_percent += result_accuracies[i]
-			temporary_percent /= index
-			print("TempPercent: ", temporary_percent)
-			if index > 0 :
-				order_count.text = str(index)
-				accuracy.text = str(snapped((temporary_percent * 100), 0.01)) + "%"
-			spawning_receipt_count -= 1
-			if spawning_receipt_count < 0:
-				spawning_receipts = false
-				accuracy.text = str(snapped((result_percent * 100), 0.01)) + "%"
-				next_animation_step()
+		_process_receipts()
 	if typing_scorelabel:
-		if typing_scorelabel_charcount != scorelabel.visible_characters:
-			typing_scorelabel_charcount = scorelabel.visible_characters
-			if scorelabel.text.right(1) != " ":
-				jitter.jitter()
-			if typing_scorelabel_charcount <= -1:
-				typing_scorelabel = false
+		_process_typing()
+	if counting_money and spawning_receipt_count >= 0:
+		_process_money()
 
 
 func display_results(accuracies : PackedFloat32Array, game_score : int):
@@ -93,6 +135,9 @@ func display_results(accuracies : PackedFloat32Array, game_score : int):
 	spawning_receipts = false
 	spawning_receipt_count = -1
 	typing_scorelabel = false
+	counting_money = false
+	for child in register.get_children():
+		child.queue_free()
 	##
 	result_order_count = accuracies.size()
 	for a in accuracies:
@@ -131,6 +176,8 @@ func next_animation_step():
 		print("Executing Step: ", timeline[next])
 		if timeline[next] == "!spawn_receipts":
 			start_spawning_receipts()
+		if timeline[next] == "!count_money":
+			start_counting_money()
 	animation_step = next
 
 
@@ -145,3 +192,10 @@ func start_typing_scorelabel():
 	print("Typing ScoreLabel")
 	typing_scorelabel = true
 	typing_scorelabel_charcount = scorelabel.visible_characters
+
+
+func start_counting_money():
+	print("Counting Money")
+	spawning_receipt_count = result_order_count
+	counting_money = true
+	tick = 0
