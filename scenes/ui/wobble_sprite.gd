28659e0381c46d6fc45ae843ed384cc5f70eb3ae
Matthew LettucePie <thelettucepie@gmail.com>
2026-01-09 13:43:02 -0500

    import title into main scene

--- scenes/ui/wobble_sprite.gd
+++ scenes/ui/wobble_sprite.gd
@@ -10,5 +10,5 @@ func tweenit():
 	var target = randf_range(deg_to_rad(-10), deg_to_rad(10))
 	var time = randf_range(1, 1.5)
 	var tween = create_tween()
-	tween.tween_property(self, "rotation", Vector3(0, target, 0), time)
+	tween.tween_property(self, "rotation", Vector3(0, target, 0), time).set_trans(Tween.TRANS_SINE)
 	tween.tween_callback(tweenit)
