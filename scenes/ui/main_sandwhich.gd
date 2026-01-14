4a47e8f692e92dd9d1881a59ba31c65f636898d6
Matthew LettucePie <thelettucepie@gmail.com>
2026-01-14 16:21:23 -0500

    cleaning up main menu sandwhich functionality

--- scenes/ui/main_sandwhich.gd
+++ scenes/ui/main_sandwhich.gd
@@ -1,12 +1,41 @@
 extends Control
+class_name MainSandwhich
 
 @onready var stack : Array = $Sandwhich.get_children()
 var current_stack : int = 0
-
+var stack_max : int = 8
+var active : bool = true
+@export var label_container : PanelContainer
+@export var label : Label
+var label_tween : Tween = null
+var stack_labels_internal : PackedStringArray = [
+	"Quit",
+	"Credits",
+	"Jukebox",
+	"Help",
+	"Options",
+	"Records",
+	"Customer-Dex",
+	"Play",
+	""
+]
+var stack_labels_translated : PackedStringArray = [
+	"Quit",
+	"Credits",
+	"Jukebox",
+	"Help",
+	"Options",
+	"Records",
+	"Customer-Dex",
+	"Play",
+	""
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	current_stack = stack.size() - 1 
+	current_stack = stack.size() - 1
+	_fill_label(current_stack)
+	go_to_layer("")
 
 
 func _tween_layer_in(obj : Sprite2D):
@@ -30,17 +59,45 @@ func _tween_layer_out(obj : Sprite2D):
 func _adjust_stack(arg : int):
 	print("Adjusting Stack from: ", current_stack, " by: ", arg)
 	var layer : Sprite2D = stack[current_stack]
-	if arg < 0:
+	if arg < 0 and current_stack != 0:
 		_tween_layer_out(layer)
-	elif arg > 0:
+		current_stack += arg
+	elif arg > 0 and current_stack != stack_max:
+		current_stack += arg
+		layer = stack[current_stack]
 		_tween_layer_in(layer)
-	current_stack = clamp(current_stack + arg, 0, stack.size() - 1)
+	_fill_label(current_stack)
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.pressed:
-			if event.keycode == KEY_DOWN:
-				_adjust_stack(-1)
-			elif event.keycode == KEY_UP:
-				_adjust_stack(1)
+func _fill_label(idx : int) -> void:
+	label.text = stack_labels_translated[idx]
+	if label.text != "":
+		label_container.show()
+	else:
+		label_container.hide()
+	label.visible_ratio = 0.0
+	if label_tween != null:
+		label_tween.kill()
+	label_tween = create_tween()
+	label_tween.tween_property(label, "visible_ratio", 1.0, 0.4)
+
+
+func _process(delta: float) -> void:
+	if active:
+		if Input.is_action_just_pressed("down") \
+		or Input.is_action_just_pressed("left"):
+			_adjust_stack(-1)
+		elif Input.is_action_just_pressed("up") \
+		or Input.is_action_just_pressed("right"):
+			_adjust_stack(1)
+
+
+func go_to_layer(layer_name : String) -> void:
+	var target_idx = stack_labels_internal.find(layer_name)
+	var difference : int = abs(current_stack - target_idx)
+	var direction : int = -1
+	print("Going to Layer: ", layer_name, " at idx: ", target_idx)
+	if target_idx > current_stack:
+		direction = 1
+	for i in difference:
+		_adjust_stack(direction)
