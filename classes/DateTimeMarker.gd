b208a241a8bbc88ce3808353ff0402772f7cc814
Matthew LettucePie <thelettucepie@gmail.com>
2026-06-21 22:40:06 -0400

    hardcode table approach to schedule

--- classes/DateTimeMarker.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name DateTimeMarker
-
-@export var weekday : Time.Weekday = Time.Weekday.WEEKDAY_SUNDAY
-@export var hour : int = 0
--- classes/TimeSlot.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name TimeSlot
-
-@export var range_a : DateTimeMarker
-@export var range_b : DateTimeMarker
