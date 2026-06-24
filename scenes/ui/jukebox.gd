eb6b6a6c7328517fa5e4624b91cdb03380ec0a0a
Matthew LettucePie <thelettucepie@gmail.com>
2026-06-24 12:18:56 -0400

    jukebox play pause

--- scenes/ui/jukebox.gd
+++ scenes/ui/jukebox.gd
@@ -6,17 +6,30 @@ class_name Jukebox
 	"break", "burgert 1", "burgert 2", "burgert 3", "BurgerFlippin'", "alphabetprimenumber"
 ]
 var current_track : int = 0
-var playback : AudioStreamPlayback
+var current_playback : float = 0
 @onready var player : AudioStreamPlayer = $Player
 @onready var track_label : Label = $screen/track_title
 @onready var jukebox_progress : JukeboxProgress = $screen/progress
+##
+##
+@onready var play_button : TextureButton = $controls/play
+@onready var play_texture : Texture = preload("res://assets/images/graphics/menu/jukebox/jukebox_interface_play.png")
+@onready var play_highlight_texture : Texture = preload("res://assets/images/graphics/menu/jukebox/jukebox_interface_play_highlight.png")
+@onready var pause_texture : Texture = preload("res://assets/images/graphics/menu/jukebox/jukebox_interface_pause.png")
+@onready var pause_highlight_texture : Texture = preload("res://assets/images/graphics/menu/jukebox/jukebox_interface_pause_highlight.png")
 
+
+func ready_jukebox() -> void:
+	player.stop()
+	current_track = 0
+	_update_player()
+	_update_play_button()
+	play_button.grab_focus()
 
 
 func _ready() -> void:
 	if get_window().get_child(0) == self:
-		$controls/prev.grab_focus()
-	player.stream = track_files[current_track]
+		ready_jukebox()
 
 
 func _on_prev_pressed() -> void:
@@ -24,13 +37,29 @@ func _on_prev_pressed() -> void:
 	if current_track < 0:
 		current_track = track_files.size() - 1
 	_update_player()
+	jukebox_progress.reset_progress()
+
+
+func _update_play_button() -> void:
+	var normal : Texture = play_texture
+	var highlight : Texture = play_highlight_texture
+	if player.playing:
+		normal = pause_texture
+		highlight = pause_highlight_texture
+	play_button.texture_normal = normal
+	play_button.texture_pressed = highlight
+	play_button.texture_hover = highlight
+	play_button.texture_focused = highlight
 
 
 func _on_play_pressed() -> void:
 	print("Play Pressed")
-	_update_player()
-	#player.play()
-	player.playing = true
+	#_update_player()
+	if player.playing:
+		player.stop()
+	else:
+		player.play(current_playback)
+	_update_play_button()
 
 
 func _on_next_pressed() -> void:
@@ -38,6 +67,7 @@ func _on_next_pressed() -> void:
 	if current_track > track_files.size() - 1:
 		current_track = 0
 	_update_player()
+	jukebox_progress.reset_progress()
 
 
 func _on_stop_pressed() -> void:
@@ -47,14 +77,13 @@ func _on_stop_pressed() -> void:
 func _update_player() -> void:
 	player.stream = track_files[current_track]
 	track_label.text = str(current_track + 1) + " - " + track_titles[current_track]
+	current_playback = 0
 
 
 func _update_progress() -> void:
 	var max : float = player.stream.get_length()
-	var current : float = player.get_playback_position()
-	#var max_digitized : int = max / 28
-	var progress : float = current / max
-	print("max: ", max, " current: ", current, " progress: ", progress)
+	current_playback = player.get_playback_position()
+	var progress : float = current_playback / max
 	var current_digitized : int = ceili(progress * 28)
 	jukebox_progress.set_progress(current_digitized)
 
