cb693c5b1c1c218cc1de99315dc0f4faab03ae00
Matthew LettucePie <thelettucepie@gmail.com>
2026-06-25 13:57:48 -0400

    export dex cleanup

--- exports/html/index.service.worker.js
+++ exports/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782333662|1348202796';
+const CACHE_VERSION = '1782410222|7957211255';
 /** @type {string} */
 const CACHE_PREFIX = 'burgert-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
