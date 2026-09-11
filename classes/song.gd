61fbd07813dcb7a8c63f3dca0f2099fd082b4ab6
Matthew LettucePie <thelettucepie@gmail.com>
2026-09-11 03:00:26 -0400

    crossfading music player

--- classes/song.gd
+++ classes/song.gd
@@ -4,3 +4,4 @@ class_name Song
 @export var track : AudioStreamMP3 = null
 @export var title : String = "title"
 @export var artist : String = "artist"
+@export var crossfade : bool = true
--- scenes/music.gd
+++ scenes/music.gd
@@ -2,24 +2,25 @@ extends AudioStreamPlayer
 class_name Music
 
 
-@export var menu : AudioStreamMP3
-@export var tracks : Array[AudioStreamMP3]
 @export var all_songs : Array[Song] = []
 var owned_songs : PackedInt32Array = [2, 3, 5, 8, 11, 12]
 var main_playlist : PackedInt32Array = [2, 8, 12]
 var play_playlist : PackedInt32Array = [3, 5, 11]
 var jukebox_playlist : PackedInt32Array = []
-@onready var anim : AnimationPlayer = $anim
 
+@onready var anim : AnimationPlayer = $anim
 enum STATE {MENU, PLAY, PAUSE}
 var current_state : STATE = STATE.MENU
+var current_song : Song = null
 
 var playback_time : float = -0
+@onready var audio_timer : Timer = $audio_timer
 
 
 func set_state(new_state : STATE):
 	if new_state == STATE.MENU:
 		anim.play("intro_ramp")
+		print("MUSIC: intro_ramp")
 	if new_state == STATE.PLAY:
 		if current_state == STATE.MENU:
 			anim.play("start_play")
@@ -35,17 +36,32 @@ func set_state(new_state : STATE):
 
 
 func set_track_random():
-	var options = tracks.duplicate()
-	if options.has(stream):
-		options.erase(stream)
-	stream = options.pick_random()
+	print("MUSIC: set_track_random")
+	var matched_song : Song = all_songs.front()
+	if current_state == STATE.MENU:
+		var idx = main_playlist[randi_range(0, main_playlist.size() - 1)]
+		matched_song = all_songs[idx]
+	if current_state == STATE.PLAY:
+		var idx = play_playlist[randi_range(0, play_playlist.size() - 1)]
+		matched_song = all_songs[idx]
+	stream = matched_song.track
+	current_song = matched_song
+	audio_timer.start(stream.get_length() - 2.0)
 	play()
 
 
 func _on_finished():
 	anim.play("queue_next")
+	print("MUSIC: queue_next")
 
 
 func capture_playback_time():
 	playback_time = get_playback_position()
 	stop()
+
+
+func _on_audio_timer_timeout() -> void:
+	audio_timer.stop()
+	if current_song.crossfade:
+		print("MUSIC: fade_out")
+		anim.play("fade_out")
